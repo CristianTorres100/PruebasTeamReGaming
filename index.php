07bb92b0edb27a7bd6e339a0dac7a06030a04728
CristianTorres100 <torresgarcia530@gmail.com>
2020-10-08 18:10:00 -0500

    Primer commit by Cristian

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title></title>
+</head>
+<body>
+
+<a href="P1_AI1.php">P1_AI1 - Variable</a> 
+<br><br>
+<a href="P1_AI2.php">P1_AI2 - Comparaciones</a> 
+<br><br>
+<a href="P1_AI3.php">P1_AI3 - Calculos</a> 
+<br><br>
+<a href="P1_AI4.php">P1_AI4 - Ciclos (arreglos)</a> 
+<br><br>
+<a href="P1_AI5.php">P1_AI5 - Pirámides</a>
+<br><br>
+
+
+</body>
+</html>
